ceb5e1ed88e1f647e7cbb45b6e6b3fc871035bfa
Charles Nurse <charlesnurse@gmail.com>
2021-05-29 14:57:26 -0700

    Move non-Blog Models from Naif.Blog.Core

---
+++ src/Naif.Core/Models/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Naif.Core.Models
+{
+    public class MenuItem
+    {
+        public string Action { get; set; }
+        
+        public string Controller { get; set; }
+        
+        public bool IsActive { get; set; }
+        
+        public string Link { get; set; }
+
+        public string Text { get; set; }
+    }
+}
