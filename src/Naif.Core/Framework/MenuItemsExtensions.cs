c011d644b2ec02bcd3d2a7ddf45ca568018a0cf2
Charles Nurse <charlesnurse@gmail.com>
2021-10-13 15:30:47 -0700

    Add MenuItemExtensions and up version to 3.3.1-alpha.1

---
+++ src/Naif.Core/Framework/MenuItemsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Naif.Core.Models;
+
+namespace Naif.Core.Framework
+{
+    public static class MenuItemsExtensions
+    {
+        public static bool ContainsIsActive(this IList<MenuItem> items)
+        {
+            bool containsIsActive = false;
+            foreach (var item in items)
+            {
+                if (item.IsActive)
+                {
+                    containsIsActive = true;
+                    break;
+                }
+            }
+
+            //Try next level
+            if (!containsIsActive)
+            {
+                foreach (var item in items)
+                {
+                    if (item.Items.ContainsIsActive())
+                    {
+                        containsIsActive = true;
+                    }
+                }
+            }
+
+            return containsIsActive;
+        }
+    }
+}
