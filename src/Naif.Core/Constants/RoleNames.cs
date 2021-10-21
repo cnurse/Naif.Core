6f95d8e23b29c5e52bc58499ba55b706107dbff9
Charles Nurse <charlesnurse@gmail.com>
2021-10-21 12:40:41 -0700

    Update RoleNames Constants Up version to 3.3.1-alpha.4

--- src/Naif.Core/Constants/RoleNames.cs
+++ src/Naif.Core/Constants/RoleNames.cs
@@ -2,7 +2,7 @@ namespace Naif.Core.Constants
 {
     public class RoleNames 
     {
-        public const string Admin = "Administrators";
+        public const string Admin = "Admin";
         public const string Everyone = "All Users";
         public const string Registered = "Registered Users";
     }
