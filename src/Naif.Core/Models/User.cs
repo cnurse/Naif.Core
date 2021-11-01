28ab3d4bed17a8494a24d47b2c5152cabde36b34
Charles Nurse <charlesnurse@gmail.com>
2021-11-01 13:59:54 -0700

    Add additional fields to User model Up version to 3.3.1-beta.3

--- src/Naif.Core/Models/User.cs
+++ src/Naif.Core/Models/User.cs
@@ -6,17 +6,35 @@ namespace Naif.Core.Models
     {
         public User()
         {
+            Created = System.DateTime.MinValue;
             Roles = new List<Role>();
+            LastUpdated = System.DateTime.MinValue;
         }
         
+        public System.DateTime Created {get; set;}
+        
         public string EmailAddress { get; set; }
+        
+        public bool EmailVerified {get; set; }
+        
+        public string GivenName {get; set;}
+        
+        public string Identifier {get; set; }
 
         public bool IsAuthenticated { get; set; }
         
+        public DateTime LastUpdated {get; set;}
+        
+        public string Locale {get; set;}
+        
+        public string NickName { get; set; }
+
         public string Name { get; set; }
         
         public string ProfileImage { get; set; }
         
         public IList<Role> Roles { get; set; }
+        
+        public string Surname {get; set; }
     }
 }
